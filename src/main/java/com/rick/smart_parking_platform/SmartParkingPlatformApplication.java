1c46e3e3ef175d03e0ac229174fa5d50498b8dd8
Henrique Alves <rickavles@gmail.com>
2026-06-11 10:58:01 -0300

    chore: initialize spring boot project

---
+++ src/main/java/com/rick/smart_parking_platform/SmartParkingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.rick.smart_parking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rick/smart_parking_platform/SmartParkingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.smart_parking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
