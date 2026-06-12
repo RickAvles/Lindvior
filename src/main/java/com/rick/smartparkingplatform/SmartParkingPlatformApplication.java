8a8b2443f45ec1aea5ad0acc980180c18df457e3
Henrique Alves <rickavles@gmail.com>
2026-06-12 12:14:18 -0300

    chore: configure local development enviroment

--- src/main/java/com/rick/smartparkingplatform/SmartParkingPlatformApplication.java
+++ src/main/java/com/rick/smartparkingplatform/SmartParkingPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.rick.smart_parking_platform;
+package com.rick.smartparkingplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
